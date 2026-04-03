b9c5a261e94bdfba9bdd95ae37d85759ebb3d60d
Alan Keating <keating.alan@gmail.com>
2026-04-03 11:50:29 +1100

    Fixed building, download scoregen listener from overview page

---
+++ build go.bat
@@ -0,0 +1,13 @@
+
+::go install github.com/akavel/rsrc@latest
+rsrc -manifest scoring_listener\rsrc.manifest -ico scoring_listener\app.ico -o scoring_listener\rsrc.syso
+
+@set GOOS=windows
+@set GOARCH=amd64
+go build -ldflags="-H windowsgui" .\scoring_listener\ 
+::Copy so that executable will be available for download from the server later
+copy scoring-listener.exe video_server\static\scoring-listener.exe
+
+rsrc -manifest video_server\rsrc.manifest -ico video_server\app.ico -o video_server\rsrc.syso
+
+go build .\video_server\
--- scoring_listener/build go.bat
+++
@@ -1,7 +0,0 @@
-
-::go install github.com/akavel/rsrc@latest
-rsrc -manifest rsrc.manifest -ico app.ico -o rsrc.syso
-
-@set GOOS=windows
-@set GOARCH=amd64
-go build -ldflags="-H windowsgui" 
